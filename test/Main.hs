b09e432ed30c5d8a6c99d35c27ddfc7a6f41d647
Henry Till <henrytill@gmail.com>
2025-07-28 15:46:08 -0700

    add initial work

--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,13 @@
 module Main (main) where
 
+import Commonmark.InitialTest qualified as InitialTest
+import Control.Monad (unless)
+import System.Exit (exitFailure)
+import Text.Printf (printf)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = do
+  let (initialOutput, initialPassed) = InitialTest.results
+  putStr initialOutput
+  printf "Summary: %s\n" (if initialPassed then "All tests passed!" else "Some tests failed.")
+  unless initialPassed exitFailure
